5f4f9aa12c1e5e3e5b9822c851987aa713c7617f
Akila_Ayanthi <aayanthi@gmail.com>
2022-02-22 23:45:45 +0530

    mytest added

--- model_weights/download_weights.sh
+++ model_weights/download_weights.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-wget -O "/home/dissana8/TOG/model_weights/FRCNN.pth" "https://www.dropbox.com/s/92q4pyzd9ve51le/FRCNN.pth?dl=0"
+# wget -O "/home/dissana8/TOG/model_weights/FRCNN.pth" "https://www.dropbox.com/s/92q4pyzd9ve51le/FRCNN.pth?dl=0"
+wget -O "/home/dissana8/TOG/model_weights/ssd512.pth" "https://www.dropbox.com/s/sqlzud8lajz4nt1/SSD512.h5?dl=0"
