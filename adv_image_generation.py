f045465b71e1b34650c44cb3284cc121ecee9eb8
Akila_Ayanthi <aayanthi@gmail.com>
2022-02-23 21:58:09 +0530

    adv image generation

--- adv_image_generation.py
+++ adv_image_generation.py
@@ -18,7 +18,13 @@ eps_iter = 2 / 255.  # Hyperparameter: attack learning rate
 n_iter = 10          # Hyperparameter: number of attack iterations
 
 fpath = '/home/dissana8/LAB/Visor/'
+savepath = '/home/dissana8/TOG/Adv_images'
 
 for path, subdirs, files in os.walk(fpath):
     for name in files:
         print(os.path.join(path, name))
+        print(name)
+        # input_img = Image.open(os.path.join(path, name))
+        # x_query, x_meta = letterbox_image_padded(input_img, size=detector.model_img_size)
+        # x_adv_untargeted = tog_untargeted(victim=detector, x_query=x_query, n_iter=n_iter, eps=eps, eps_iter=eps_iter)
+        # x_adv_untargeted = x_adv_untargeted.save(os.path.join(savepath, )))
