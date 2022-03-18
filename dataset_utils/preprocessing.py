465d3d39e923dca2fec2e2d850a86e5c76a728c8
Akila_Ayanthi <aayanthi@gmail.com>
2022-03-18 11:12:59 +0530

    generate adv images

--- dataset_utils/preprocessing.py
+++ dataset_utils/preprocessing.py
@@ -5,17 +5,11 @@ import numpy as np
 def letterbox_image_padded(image, size=(416, 416)):
     """ Resize image with unchanged aspect ratio using padding """
     image_copy = image.copy()
-    print(np.asarray(image_copy))
     iw, ih = image_copy.size
-    print(iw)
-    print(ih)
     w, h = size
     scale = min(w / iw, h / ih)
-    print(scale)
     nw = int(iw * scale)
     nh = int(ih * scale)
-    print(nw)
-    print(nh)
 
     image_copy = image_copy.resize((416, 416), Image.BICUBIC)
     # print(np.asarray(image_copy))
@@ -26,7 +20,6 @@ def letterbox_image_padded(image, size=(416, 416)):
     # print(np.asarray(new_image))
     # new_image = np.asarray(new_image)[np.newaxis, :, :, :] / 255.
     new_image = np.asarray(image_copy)[np.newaxis, :, :, :] / 255.
-    print(new_image)
     meta = ((w - nw) // 2, (h - nh) // 2, nw + (w - nw) // 2, nh + (h - nh) // 2, scale)
 
     return new_image, meta
