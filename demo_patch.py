345840e33f760abf46aae6f7a71dc0b1f0198bd9
Akila_Ayanthi <aayanthi@gmail.com>
2022-04-20 10:48:49 +0530

    new adv image generation

--- demo_patch.py
+++ demo_patch.py
@@ -279,11 +279,11 @@ detections_adv = detector.detect(x_adv, conf_threshold=detector.confidence_thres
 detections_rand = detector.detect(x_rand, conf_threshold=detector.confidence_thresh_default)
 x_ad = torch.tensor(x_adv)
 x_ad = x_ad.permute(0, 3, 2, 1)
-# rotated_im = rot_img(x_ad, -np.pi/2, dtype)
+rotated_im = rot_img(x_ad, -np.pi/2, dtype)
 save_image(x_ad, "adv_image3.png")
 image = Image.open("adv_image3.png")
-image = image.resize((640, 480))
 image = ImageOps.mirror(image)
+image = image.resize((640, 480))
 image.save("adv_image3.png")
 
 # adv_image = img.save("adv_image3.jpg")
