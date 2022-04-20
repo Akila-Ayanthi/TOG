c9012aed0bc1ccc2ee794606a0cc7dc8867597ee
Akila_Ayanthi <aayanthi@gmail.com>
2022-04-20 11:00:23 +0530

    new adv image generation

--- demo_patch.py
+++ demo_patch.py
@@ -280,11 +280,11 @@ detections_rand = detector.detect(x_rand, conf_threshold=detector.confidence_thr
 x_ad = torch.tensor(x_adv)
 x_ad = x_ad.permute(0, 3, 2, 1)
 rotated_im = rot_img(x_ad, -np.pi/2, dtype)
-save_image(x_ad, "adv_image3.png")
-# image = Image.open("adv_image3.png")
-# # image = ImageOps.mirror(image)
-# image = image.resize((640, 480))
-# image.save("adv_image3.png")
+save_image(rotated_im, "adv_image3.png")
+image = Image.open("adv_image3.png")
+image = ImageOps.mirror(image)
+image = image.resize((640, 480))
+image.save("adv_image3.png")
 
 # adv_image = img.save("adv_image3.jpg")
 # visualize_detections({'Benign (No Attack)': (x_query, detections_query, detector.model_img_size, detector.classes),
