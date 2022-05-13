a53ddec57a8207b1df44dc076cd89498eccc08b1
Akila_Ayanthi <aayanthi@gmail.com>
2022-05-13 08:15:57 +0530

    tog patch recheck

--- dataset_utils/preprocessing.py
+++ dataset_utils/preprocessing.py
@@ -2,6 +2,28 @@ from PIL import Image
 import numpy as np
 
 
+# def letterbox_image_padded(image, size=(416, 416)):
+#     """ Resize image with unchanged aspect ratio using padding """
+#     image_copy = image.copy()
+#     iw, ih = image_copy.size
+#     w, h = size
+#     scale = min(w / iw, h / ih)
+#     nw = int(iw * scale)
+#     nh = int(ih * scale)
+
+#     image_copy = image_copy.resize((416, 416), Image.BICUBIC)
+#     # print(np.asarray(image_copy))
+#     # new_image = Image.new('RGB', size, (0, 0, 0))
+#     # print(np.asarray(new_image))
+#     # new_image.paste(image_copy, ((w - nw) // 2, (h - nh) // 2))
+#     # print("pasted image")
+#     # print(np.asarray(new_image))
+#     # new_image = np.asarray(new_image)[np.newaxis, :, :, :] / 255.
+#     new_image = np.asarray(image_copy)[np.newaxis, :, :, :] / 255.
+#     meta = ((w - nw) // 2, (h - nh) // 2, nw + (w - nw) // 2, nh + (h - nh) // 2, scale)
+
+#     return new_image, meta
+
 def letterbox_image_padded(image, size=(416, 416)):
     """ Resize image with unchanged aspect ratio using padding """
     image_copy = image.copy()
@@ -11,15 +33,10 @@ def letterbox_image_padded(image, size=(416, 416)):
     nw = int(iw * scale)
     nh = int(ih * scale)
 
-    image_copy = image_copy.resize((416, 416), Image.BICUBIC)
-    # print(np.asarray(image_copy))
-    # new_image = Image.new('RGB', size, (0, 0, 0))
-    # print(np.asarray(new_image))
-    # new_image.paste(image_copy, ((w - nw) // 2, (h - nh) // 2))
-    # print("pasted image")
-    # print(np.asarray(new_image))
-    # new_image = np.asarray(new_image)[np.newaxis, :, :, :] / 255.
-    new_image = np.asarray(image_copy)[np.newaxis, :, :, :] / 255.
+    image_copy = image_copy.resize((nw, nh), Image.BICUBIC)
+    new_image = Image.new('RGB', size, (0, 0, 0))
+    new_image.paste(image_copy, ((w - nw) // 2, (h - nh) // 2))
+    new_image = np.asarray(new_image)[np.newaxis, :, :, :] / 255.
     meta = ((w - nw) // 2, (h - nh) // 2, nw + (w - nw) // 2, nh + (h - nh) // 2, scale)
 
     return new_image, meta
